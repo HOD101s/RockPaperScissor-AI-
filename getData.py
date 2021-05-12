e372b9a209102cdf16d2cf420e193aa8d9afb945
Manas Acharya <manasbass.99@gmail.com>
2021-05-12 15:35:08 +0530

    modified: image collection size

--- getData.py
+++ getData.py
@@ -21,9 +21,9 @@ print("Hit Space to Capture Image")
 
 while True:
     ret, frame = cap.read()
-    cv2.imshow('Get Data : '+label,frame[50:350,100:450])
+    cv2.imshow('Get Data : '+label,frame[50:350,150:450])
     if cv2.waitKey(1) & 0xFF == ord(' '):
-        cv2.imwrite(SAVE_PATH+'\\'+label+'{}.jpg'.format(ct),frame[50:350,100:450])
+        cv2.imwrite(SAVE_PATH+'\\'+label+'{}.jpg'.format(ct),frame[50:350,150:450])
         print(SAVE_PATH+'\\'+label+'{}.jpg Captured'.format(ct))
         ct+=1
     if ct >= maxCt:
